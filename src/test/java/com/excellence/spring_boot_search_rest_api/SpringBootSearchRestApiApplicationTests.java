9a988c99e8ca802cdbe30ac36a0301aedd89144f
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-07 11:46:44 +0530

    base-code-created

---
+++ src/test/java/com/excellence/spring_boot_search_rest_api/SpringBootSearchRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.spring_boot_search_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSearchRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
